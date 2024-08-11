97b307f339dfe0a6b3cabbb1f652936378cf0cd0
Aashick <50665905+Aashick@users.noreply.github.com>
2024-08-11 11:06:46 +0530

    Update 0424_Longest_Repeating_Character_Replacement.js

--- Sliding Window/0424_Longest_Repeating_Character_Replacement.js
+++ Sliding Window/0424_Longest_Repeating_Character_Replacement.js
@@ -17,18 +17,18 @@ Output: 5
    * @return {number}
    */
   characterReplacement(s, k) {
-    let frequencyMap = new Map(); // Map to store the frequency of characters in the current window.
+    let charCount = new Map(); // Map to store the frequency of characters in the current window.
     let l = 0;          // Left pointer of the sliding window.
     let maxFrequency = 0;         // Maximum frequency of any single character in the current window.
     let substringWindow = 0; // Length of the longest valid window found.
 
     for (let r = 0; r < s.length; r++) { // Iterate with the right pointer over the string.
-      frequencyMap.set(s[r], (frequencyMap.get(s[r]) || 0) + 1); // Increment the count of the current character.
-      maxFrequency = Math.max(maxFrequency, frequencyMap.get(s[r])); // Update maxFrequency to be the highest frequency of any character in the current window.
+      charCount.set(s[r], (charCount.get(s[r]) || 0) + 1); // Increment the count of the current character.
+      maxFrequency = Math.max(maxFrequency, charCount.get(s[r])); // Update maxFrequency to be the highest frequency of any character in the current window.
 
       while (r - l + 1 - maxFrequency > k) { // Check if the current window size minus the highest frequency exceeds k. (r - l + 1 => Current Window)
-        frequencyMap.set(s[l], frequencyMap.get(s[l]) - 1); // Decrease the frequency count of the character at the left pointer.
-        if (frequencyMap.get(s[l]) === 0) frequencyMap.delete(s[l]); // If the count drops to zero, remove the character from the map.
+        charCount.set(s[l], charCount.get(s[l]) - 1); // Decrease the frequency count of the character at the left pointer.
+        if (charCount.get(s[l]) === 0) charCount.delete(s[l]); // If the count drops to zero, remove the character from the map.
         l++; // Move the left pointer to the right to shrink the window.
       }
 
