1ac84c7f21867d93385a163c104a5b3691b5d3b8
Aashick <aashickn@gmail.com>
2025-01-04 11:29:32 +0530

    4 jan

---
+++ AlgoMaster/01_Arrays/first-missing-positive.go
@@ -0,0 +1,27 @@
+func firstMissingPositive(nums []int) int {
+	n := len(nums)
+
+	// Place each number in its correct position
+	for i := 0; i < n; i++ {
+		for nums[i] > 0 && nums[i] <= n && nums[i] != nums[nums[i]-1] {
+			// Swap nums[i] with nums[nums[i]-1]
+			nums[nums[i]-1], nums[i] = nums[i], nums[nums[i]-1]
+		}
+	}
+
+	// Find the first missing positive
+	for i := 0; i < n; i++ {
+		if nums[i] != i+1 {
+			return i + 1
+		}
+	}
+
+	return n + 1
+}
+
+// Time Complexity: O(n) 
+// Space Complexity: O(1)
+
+// 41
+// https://leetcode.com/problems/first-missing-positive/description/
+
